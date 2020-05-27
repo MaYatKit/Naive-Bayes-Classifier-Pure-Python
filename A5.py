b5687421981d40f5f042ea00010e2fccb5d631d6
JimmyMa <ma417704684@gmail.com>
2020-05-27 13:38:33 +1200

    first commit

---
+++ A5.py
@@ -0,0 +1,441 @@
+import csv
+import re
+import numpy
+import math
+import os
+import time
+
+
+
+class Script:
+    my_path = os.path.abspath(os.path.dirname(__file__))
+
+    # storage chi_square_score for all words of training data
+    chi_square_score = os.path.join(my_path, "chi_square_score.txt")
+
+    # storage the most 1000 important word to save time
+    most_important = os.path.join(my_path, "most_important.txt")
+    train_file = os.path.join(my_path, "trg.csv")
+    test_file = os.path.join(my_path, "tst.csv")
+
+    # storage result
+    result_file = os.path.join(my_path, "result.csv")
+
+
+# Use chi-square score to preprocess the trg.csv and get the 1000 most important
+# words of 0~3600 rows of trg.csv and save them to most_important.txt
+def dataPreprocess():
+    print("Script started ... ")
+
+    args_each = [None] * 3600  # storage number of words in each line
+    args = set()  # storage all words, can filter out duplicate words.
+    classes = {}  # storage classes and their count
+    B_each_row = []
+    A_each_row = []
+    E_each_row = []
+    V_each_row = []
+    regexp = re.compile(r'^([^(?!a-z|A-Z)])*$')  # filter out all non-letter elements.
+    with open(Script.train_file) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line = 0
+        for row in csv_reader:
+            if line == 0:
+                line = 1
+                continue
+            if line == 3601:
+                break
+            if classes.keys().__contains__(row[1]):
+                classes[row[1]] = classes[row[1]] + 1
+            else:
+                classes[row[1]] = 1
+            document = row[2]  # the 3rd column is a document
+            docs = document.split(r' ')
+
+            # filter out if words are only-number, '' and any not includes letters
+            real_docs = [doc for doc in docs if doc != '' and not regexp.search(doc) and not doc.isdigit()]
+            for doc in real_docs:
+                args.add(doc)  # collect all words
+            wrapper = numpy.array(real_docs)
+            unique, counts = numpy.unique(wrapper, return_counts=True)
+            dic = dict(zip(unique, counts))
+            if row[1] is 'B':
+                B_each_row.append(dic)
+            elif row[1] is 'V':
+                V_each_row.append(dic)
+            elif row[1] is 'A':
+                A_each_row.append(dic)
+            elif row[1] is 'E':
+                E_each_row.append(dic)
+            args_each[line - 1] = dic
+            line = line + 1
+
+    # now, all words are are recorded in the args,
+    # and we need to statistic how many occurrence for each frequency for each words
+    occurrence_frequency = {}
+    chi_square_score = {}
+    #  apply chi-square to filter the most 1000 important words
+    for each in args_each:  # iterate each row of document
+        for key in each:  # iterate each word's frequency
+            if not occurrence_frequency.keys().__contains__(key):
+                occurrence_frequency[key] = {}
+            if occurrence_frequency[key].keys().__contains__(
+                    each[key]):  # e.g. this row we found five 'the', so the frequency of '5-the' +1
+                occurrence_frequency[key][each[key]] = occurrence_frequency[key][each[key]] + 1
+            else:
+                occurrence_frequency[key][each[key]] = 1
+
+    # supply non-exist words as zero in each row.
+    for of in occurrence_frequency.values():
+        word_sum = 0
+        for num in of.values():
+            word_sum = word_sum + num
+        of[0] = 3600 - word_sum
+
+    for key in occurrence_frequency.keys():  # 'occurrence_frequency' storages the frequency of
+        # all '0-the', '1-the', '2-the', '0-a', '1-a', ..., for all words
+        word_and_of = occurrence_frequency[key]
+        chi_score = 0
+        for num in word_and_of.keys():
+            xi = 0
+            oijA = 0
+            if num == 0:  # e.g., '0-the', if this row of A class has no '0-the' then the frequency of '0-the' + 1
+                for row in A_each_row:
+                    if key not in row:
+                        oijA = oijA + 1
+            else:
+                for row in A_each_row:
+                    if key in row and num == row[key]:
+                        oijA = oijA + 1
+            eijA = (word_and_of[num] / 3600) * (classes['A'] / 3600) * 3600
+            xi = (oijA - eijA) ** 2 / eijA
+            oijB = 0
+            if num == 0:
+                for row in B_each_row:
+                    if key not in row:
+                        oijB = oijB + 1
+            else:
+                for row in B_each_row:
+                    if key in row and num == row[key]:
+                        oijB = oijB + 1
+            eijB = (word_and_of[num] / 3600) * (classes['B'] / 3600) * 3600
+            xi = xi + (oijB - eijB) ** 2 / eijB
+            oijV = 0
+            if num == 0:
+                for row in V_each_row:
+                    if key not in row:
+                        oijV = oijV + 1
+            else:
+                for row in V_each_row:
+                    if key in row and num == row[key]:
+                        oijV = oijV + 1
+            eijV = (word_and_of[num] / 3600) * (classes['V'] / 3600) * 3600
+            xi = xi + (oijV - eijV) ** 2 / eijV
+            oijE = 0
+            if num == 0:
+                for row in E_each_row:
+                    if key not in row:
+                        oijE = oijE + 1
+            else:
+                for row in E_each_row:
+                    if key in row and num == row[key]:
+                        oijE = oijE + 1
+            eijE = (word_and_of[num] / 3600) * (classes['E'] / 3600) * 3600
+            xi = xi + (oijE - eijE) ** 2 / eijE
+            chi_score = chi_score + xi
+        chi_square_score[key] = chi_score
+
+    chi_square_score = {k: v for k, v in sorted(chi_square_score.items(), key=lambda item: item[1], reverse=True)}
+    f = open(Script.chi_square_score, "a")  # save the max 1000 words into most_important.txt.
+    for chi_key in chi_square_score.keys():
+        f.write(str(chi_key) + " : " + str(chi_square_score[chi_key]) + '\n')
+    f.close()
+    most_important = [0] * 1000
+    index = 0
+    for key in chi_square_score.keys():
+        most_important[index] = key
+        if index == 999:
+            break
+        else:
+            index = index + 1
+
+    f = open(Script.most_important, "a")
+    for word in most_important:
+        f.write(str(word) + '\n')
+    f.close()
+    return most_important
+
+
+def NaiveBayesClassifier(most_important, validation_set, ground_truth,  is_validation):
+    # save all Conditional Probabilities for all classes
+    cp_a = {}
+    cp_e = {}
+    cp_v = {}
+    cp_b = {}
+
+    # get all words count on each row
+    args = set()  # storage all words, can filter out duplicate words.
+    B_each_row = []
+    A_each_row = []
+    E_each_row = []
+    V_each_row = []
+    regexp = re.compile(r'^([^(?!a-z|A-Z)])*$')  # filter out all non-letter elements.
+    with open(Script.train_file) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line = 0
+        for row in csv_reader:
+            if line == 0:
+                line = 1
+                continue
+            if line >= 3601:
+                break
+            document = row[2]  # the 3rd column is a document
+            docs = document.split(r' ')
+
+            # filter out if words are only-number, '' and any not includes letters
+            real_docs = [doc for doc in docs if doc != '' and not regexp.search(doc) and not doc.isdigit()]
+            for doc in real_docs:
+                args.add(doc)  # collect all words
+            wrapper = numpy.array(real_docs)
+            unique, counts = numpy.unique(wrapper, return_counts=True)
+            dic = dict(zip(unique, counts))
+            if row[1] is 'B':
+                B_each_row.append(dic)
+            elif row[1] is 'V':
+                V_each_row.append(dic)
+            elif row[1] is 'A':
+                A_each_row.append(dic)
+            elif row[1] is 'E':
+                E_each_row.append(dic)
+            line = line + 1
+
+    # count words number in each class
+    words_num_a = 0
+    words_num_b = 0
+    words_num_e = 0
+    words_num_v = 0
+
+    # how many classes
+    num_a = len(A_each_row) / 3600
+    num_b = len(B_each_row) / 3600
+    num_v = len(V_each_row) / 3600
+    num_e = len(E_each_row) / 3600
+
+    # count word number in each class
+    for row in A_each_row:
+        for key in row.keys():
+            if key in most_important:
+                words_num_a = words_num_a + row[key]
+
+    for row in B_each_row:
+        for key in row.keys():
+            if key in most_important:
+                words_num_b = words_num_b + row[key]
+
+    for row in E_each_row:
+        for key in row.keys():
+            if key in most_important:
+                words_num_e = words_num_e + row[key]
+
+    for row in V_each_row:
+        for key in row.keys():
+            if key in most_important:
+                words_num_v = words_num_v + row[key]
+
+    version_space = len(most_important)
+
+    normalised_count_A = [0] * len(A_each_row)  # transform based on length example in class A
+    normalised_count_B = [0] * len(B_each_row)  # transform based on length example in class B
+    normalised_count_E = [0] * len(E_each_row)  # transform based on length example in class E
+    normalised_count_V = [0] * len(V_each_row)  # transform based on length example in class V
+
+    index = 0
+    for row in A_each_row:
+        normalised_count = 0
+        for key in row.keys():
+            if key in most_important:
+                normalised_count = normalised_count + row[key] ** 2
+        normalised_count = math.sqrt(normalised_count)
+        normalised_count_A[index] = normalised_count
+        index = index + 1
+
+    index = 0
+    for row in B_each_row:
+        normalised_count = 0
+        for key in row.keys():
+            if key in most_important:
+                normalised_count = normalised_count + row[key] ** 2
+        normalised_count = math.sqrt(normalised_count)
+        normalised_count_B[index] = normalised_count
+        index = index + 1
+
+    index = 0
+    for row in E_each_row:
+        normalised_count = 0
+        for key in row.keys():
+            if key in most_important:
+                normalised_count = normalised_count + row[key] ** 2
+        normalised_count = math.sqrt(normalised_count)
+        normalised_count_E[index] = normalised_count
+        index = index + 1
+
+    index = 0
+    for row in V_each_row:
+        normalised_count = 0
+        for key in row.keys():
+            if key in most_important:
+                normalised_count = normalised_count + row[key] ** 2
+        normalised_count = math.sqrt(normalised_count)
+        normalised_count_V[index] = normalised_count
+        index = index + 1
+
+    # count likelihood for words in each class
+    for word in most_important:
+        num = 0
+        index = 0
+        for row in A_each_row:
+            if row.keys().__contains__(word):
+                # num = num + (row[word] / normalised_count_A[index])  # downweight by the word occurrence in this document.
+                num = num + (row[word] / 1)  # downweight by the word occurrence in this document.
+            index = index + 1
+        cp_each = (num + 1) / (words_num_a + version_space)
+        cp_a[word] = cp_each
+
+        num = 0
+        index = 0
+        for row in B_each_row:
+            if row.keys().__contains__(word):
+                num = num + (row[word] / 1)  # downweight by the word occurrence in this document.
+            index = index + 1
+        cp_each = (num + 1) / (words_num_b + version_space)
+        cp_b[word] = cp_each
+
+        num = 0
+        index = 0
+        for row in E_each_row:
+            if row.keys().__contains__(word):
+                num = num + (row[word] / 1)  # downweight by the word occurrence in this document.
+            index = index + 1
+        cp_each = (num + 1) / (words_num_e + version_space)
+        cp_e[word] = cp_each
+
+        num = 0
+        index = 0
+        for row in V_each_row:
+            if row.keys().__contains__(word):
+                num = num + (row[word] / 1)  # downweight by the word occurrence in this document.
+            index = index + 1
+        cp_each = (num + 1) / (words_num_v + version_space)
+        cp_v[word] = cp_each
+
+    validation_result = [0] * len(validation_set)
+
+    index = 0
+    for row in validation_set:
+        # initial all conditional probabilities by class proportion
+        p_a = math.log10(num_a)
+        p_e = math.log10(num_e)
+        p_b = math.log10(num_b)
+        p_v = math.log10(num_v)
+        # p_a = num_a
+        # p_e = num_e
+        # p_b = num_b
+        # p_v = num_v
+        wrapper = numpy.array(row.split(' '))
+        unique, counts = numpy.unique(wrapper, return_counts=True)
+        dic = dict(zip(unique, counts))
+        normalised_count = 0  # transform based on length example
+        candidates = []  # save all candidates in this row
+        for cp_word in dic.keys():
+            if cp_word in most_important:
+                # normalised_count = normalised_count + dic[cp_word]**2
+                candidates.append(cp_word)
+        # normalised_count = math.sqrt(normalised_count)
+        for cp_word in candidates:
+            # multiple size of candidate words to avoid too small to overflow
+            # p_a = p_a * ((cp_a[cp_word] ** dic[cp_word])/1) * len(most_important)
+            # p_b = p_b * ((cp_b[cp_word] ** dic[cp_word])/1) * len(most_important)
+            # p_v = p_v * ((cp_v[cp_word] ** dic[cp_word])/1) * len(most_important)
+            # p_e = p_e * ((cp_e[cp_word] ** dic[cp_word])/1) * len(most_important)
+            # Use multi-nominal NBC and log the probabilities
+            p_a = p_a + dic[cp_word] * math.log10(cp_a[cp_word])
+            p_b = p_b + dic[cp_word] * math.log10(cp_b[cp_word])
+            p_v = p_v + dic[cp_word] * math.log10(cp_v[cp_word])
+            p_e = p_e + dic[cp_word] * math.log10(cp_e[cp_word])
+        if p_a > p_e and p_a > p_b and p_a > p_v:
+            validation_result[index] = 'A'
+        if p_b > p_e and p_b > p_a and p_b > p_v:
+            validation_result[index] = 'B'
+        if p_v > p_a and p_v > p_b and p_v > p_e:
+            validation_result[index] = 'V'
+        if p_e > p_a and p_e > p_b and p_e > p_v:
+            validation_result[index] = 'E'
+        index = index + 1
+
+    if is_validation:  # validation process
+        correct_num = 0
+        index = 0
+        wrong_indexs = []
+        for predict in validation_result:
+            if predict == ground_truth[index]:
+                correct_num = correct_num + 1
+            else:
+                wrong_indexs.append(index)
+            index = index + 1
+        print('correct rate of validation set is: ' + str(correct_num / len(ground_truth) * 100) + "%")
+    else:  # test process
+        print(validation_result)
+        f = open(Script.result_file, "w")
+        f.write("id,class\n")
+        index = 1
+        for result in validation_result:
+            f.write(str(index) + "," + result + '\n')
+            index = index + 1
+        f.close()
+
+
+if __name__ == '__main__':
+    # if you want to run dataPreprocess function, please ensure "Script.train_file"
+    # is pointing to trg.csv; and dataPreprocess function does not support other
+    # data set but only trg.csv.
+
+    # words = dataPreprocess()  # get the most 1000 important words, use chi-square score to select
+
+    words = []
+    f = open(Script.most_important, "r")
+    for word in f:
+        words.append(word.split('\n')[0])
+    f.close()
+
+    validation_start = 0
+    validation_set = []  # read validation set begin at 3601
+    ground_truth = []
+    with open(Script.train_file) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            if validation_start > 3600:  # use the 3601st-4000th data of trg.csv as a validation set
+                validation_set.append(row[2])
+                ground_truth.append(row[1])
+            else:
+                validation_start = validation_start + 1
+                continue
+
+    # Please set right path to "Script.train" variable, before evaluate your data,
+    # which is the trg.csv; and please load your test data into a
+    # List object, and pass it into method "NaiveBayesClassifier()" as a second
+    # parameter, and please set the ground true list to be the third parameter
+    # and the fourth parameter please set to be True
+    startTime = time.time()
+    NaiveBayesClassifier(words, validation_set, ground_truth, True)
+    endTime = time.time()
+    print("process time is " + str(endTime - startTime) + " seconds")
+
+    # test_set = []  # read test set
+    # with open(Script.test_file) as csv_file:
+    #     csv_reader = csv.reader(csv_file, delimiter=',')
+    #     for row in csv_reader:
+    #         test_set.append(row[1])
+    #
+    # if you want to test a dataset, please first load into a
+    # list and then set to be second parameter; the third and
+    # the fourth parameters please set them to be None and False
+    # NaiveBayesClassifier(words, test_set, None,  False)
---
+++ venv/lib/python3.7/site-packages/arff/__init__.py
@@ -0,0 +1,376 @@
+'''
+Weka arff file type reader for python.
+
+http://code.google.com/p/arff/
+
+http://code.google.com/p/arff/wiki/Documentation
+
+Usage:
+
+    >>> import arff
+    >>> for row in arff.load('example.arff'):
+    ...     print(row.hair_color)
+    ...     print(row[-1])
+    ...
+    >>> print(list(arff.load('example.arff')))
+    [[Row(hair_color='blonde', age=17.2, patno=1),
+     Row(hair_color='blue', age=27.2, patno=2),
+     Row(hair_color='blue', age=18.2, patno=3)]
+
+
+
+Where this is the example file:
+
+@relation diabetics_data
+@attribute hair_color {blonde, black, blue}
+@attribute age real
+@attribute patno integer
+@data
+blonde, 17.2, 1
+blue, 27.2, 2
+blue, 18.2, 3
+
+-----
+    >>> data = [[1,2,'a'], [3, 4, 'john']]
+    >>> arff.dump('result.arff', data, relation="whatever", names=['num', 'day', 'title'])
+
+results in the creation of this file:
+
+@relation whatever
+@attribute num integer
+@attribute day integer
+@attribute title string
+@data
+1,2,'a'
+3,4,'john'
+
+
+-----
+
+
+supports arff types: numeric, integer, real, string
+supports python types: int, str, float
+
+in the future: dates
+
+Tested on python 2.7 and 3.2
+
+License: BSD, do what you wish with this. Could be awesome to hear if you found
+it useful and/or you have suggestions. ubershmekel at gmail
+
+Based on http://weka.wikispaces.com/ARFF+%28stable+version%29
+
+'''
+
+import os
+import io
+import csv
+from collections import namedtuple
+
+COMMENT = '%'
+SPECIAL = '@'
+RELATION = '@relation'
+ATTRIBUTE = '@attribute'
+DATA = '@data'
+
+
+def _str_remove_quotes(obj):
+    return str(obj[1:-1])
+
+
+def GenerateRowBase(field_names):
+    """
+    Rows should behave like so:
+        * list(row) should give the values in order
+        * row['class'] should get the column named 'class'
+        * row[i] should get the i-th column
+        * row.balls should get the column named 'balls'
+    """
+    class Row:
+        def __init__(self, *values):
+            # iter access
+            self._values = list(values)
+            # names access
+            self._data = dict(zip(field_names, self._values))
+            # numbered order access
+            self._data.update(enumerate(self._values))
+
+        def __getattr__(self, key):
+            if key in self._data:
+                return self._data[key]
+            else:
+                return object.__getattr__(self, key)
+
+        def __getitem__(self, key):
+            return self._data[key]
+
+        def __repr__(self):
+            return '<Row(%s)>' % ','.join([repr(i) for i in self._values])
+
+        def __iter__(self):
+            return iter(self._values)
+
+        def __len__(self):
+            return len(self._values)
+
+    return Row
+
+ARFF_TYPES = {
+    'numeric': float,
+    'integer': int,
+    'real': float,
+    'string': _str_remove_quotes,
+}
+
+PYTHON_TYPES = {
+    float: 'real',
+    int: 'integer',
+    str: 'string',
+    bool: '{True, False}'
+}
+
+DEFAULT_REPRS = {
+                }
+
+
+def add_optional_types():
+    try:
+        import numpy
+        PYTHON_TYPES[numpy.float64] = 'real'
+        PYTHON_TYPES[numpy.int64] = 'integer'
+    except ImportError:
+        pass
+
+    try:
+        PYTHON_TYPES[long] = 'integer'
+        DEFAULT_REPRS[long] = str
+    except NameError:
+        pass
+
+    try:
+        PYTHON_TYPES[unicode] = 'string'
+        DEFAULT_REPRS[unicode] = lambda x: x.encode('utf-8')
+    except NameError:
+        pass
+
+add_optional_types()
+
+
+# python2/3 compatible unicode
+def _u(text):
+    if str == bytes:
+        return text.decode('utf-8')
+    else:
+        # python 3
+        return text
+
+
+def _csv_split(line):
+    return next(csv.reader([line]))
+
+
+class Nominal(str):
+    """Use this class to wrap strings which are intended to be nominals
+    and shouldn't have enclosing quote signs."""
+    def __repr__(self):
+        return self
+
+
+class _ParsedNominal:
+    '''Parses and validates the arff enum'''
+    def __init__(self, name, type_text):
+        self.name = name
+        self.type_text = type_text
+        values_str = type_text.strip('{} ')
+        self.enum = _csv_split(values_str)
+        self.enum = [opt.strip(', \'"') for opt in self.enum]
+
+    def parse(self, text):
+        if text.strip('\'"') in self.enum:
+            return text
+        else:
+            raise ValueError("'%s' is not in {%s}" % (text, self.enum))
+
+
+class _SimpleType:
+    def __init__(self, name, type_text):
+        self.name = name
+        self.type = ARFF_TYPES[type_text]
+
+    def parse(self, text):
+        return self.type(text)
+
+
+def _parse_types(row, fields):
+    typed_row = []
+    for i, ftype in enumerate(fields):
+        typed_row.append(ftype.parse(row[i]))
+
+    return typed_row
+
+
+class _RowParser:
+    def __init__(self, fields):
+        self.fields = fields
+        #self.tuple = namedtuple('Row', [f.name for f in fields])
+        self.rowgen = GenerateRowBase([f.name for f in fields])
+
+    def parse(self, row):
+        values = []
+        for f, item in zip(self.fields, row):
+            values.append(f.parse(item))
+
+        return self.rowgen(*values)
+
+
+def loads(text):
+    if bytes == str:
+        if not isinstance(text, unicode):
+            raise ValueError('arff.loads works with unicode strings only')
+    else:
+        if not isinstance(text, str):
+            raise ValueError('arff.loads works with strings only')
+    lines_iterator = io.StringIO(text)
+    for item in Reader(lines_iterator):
+        yield item
+
+
+def load(fname):
+    with open(fname, 'r') as fhand:
+        for item in Reader(fhand):
+            yield item
+
+
+class Reader:
+    def __init__(self, lines_iterator):
+        self.lines_iterator = lines_iterator
+        self.arfftypes = dict(ARFF_TYPES)
+
+    def __iter__(self):
+        """
+        The iterator does all the parsing so the user can customize the parser
+        right after construction before the reader does anything.
+        """
+        # if we were passed a list - make sure it behaves like an iterator
+        # as we have 2 for loops that expect to not restart.
+        lines_iterator = iter(self.lines_iterator)
+        fields = []
+
+        for line in lines_iterator:
+            if line.startswith(COMMENT):
+                continue
+
+            if line.lower().startswith(DATA):
+                break
+
+            if line.lower().startswith(RELATION):
+                _, relation = line.split(' ', 1)
+                self.relation = relation.strip('"\' ')
+
+            if line.lower().startswith(ATTRIBUTE):
+                space_separated = line.split(' ', 2)
+                name = space_separated[1]
+                field_type_text = space_separated[2].strip()
+
+                fields.append(self._field_type(name, field_type_text))
+
+        self.fields = fields
+
+        # data
+        row_parser = _RowParser(fields)
+        for line in lines_iterator:
+            if line.startswith(COMMENT):
+                continue
+            row = _csv_split(line)
+            typed_row = row_parser.parse(row)
+            yield typed_row
+
+    def _field_type(self, name, type_text):
+        if type_text.lower() in self.arfftypes:
+            return _SimpleType(name, type_text.lower())
+
+        if type_text.startswith('{'):
+            return _ParsedNominal(name, type_text)
+
+        raise ValueError("Unrecognized attribute type: %s" % type_text)
+
+        #'date': date_format,
+
+
+def _convert_row(row):
+    items = [repr(item) for item in row]
+    return ','.join(items)
+
+
+def dumps(*args, **kwargs):
+    items = []
+    rows_gen = (row for row in dump_lines(*args, **kwargs))
+    return _u(os.linesep).join(rows_gen)
+
+
+def dump_lines(row_iterator, relation='untitled', names=None):
+    w = _LineWriter(relation, names)
+    for row in row_iterator:
+        for line in w.generate_lines(row):
+            yield line
+
+
+def dump(fname, row_iterator, relation='untitled', names=None):
+    w = Writer(fname, relation, names)
+    for row in row_iterator:
+        w.write(row)
+    w.close()
+
+
+class _LineWriter:
+    def __init__(self, relation='untitled', names=None):
+        self.relation = relation
+        self.names = names
+        self._first_row = True
+        self.pytypes = dict(PYTHON_TYPES)
+
+    def generate_lines(self, row):
+        if self._first_row:
+            self._first_row = False
+            ftypes = []
+            for item in row:
+                item_type = type(item)
+                if item_type not in self.pytypes:
+                    raise ValueError("Unknown type: %s" % item_type)
+                ftypes.append(self.pytypes[item_type])
+            if self.names is None:
+                self.names = ['attr%d' % i for i in range(len(row))]
+
+            yield "%s %s" % (RELATION, self.relation)
+
+            for name, ft in zip(self.names, ftypes):
+                yield "%s %s %s" % (ATTRIBUTE, name, ft)
+
+            yield DATA
+
+        yield self._convert_row(row)
+
+    def _convert_obj(self, obj):
+        typ = type(obj)
+        if typ in DEFAULT_REPRS:
+            return DEFAULT_REPRS[typ](obj)
+        else:
+            return repr(obj)
+
+    def _convert_row(self, row):
+        items = [self._convert_obj(item) for item in row]
+        return ','.join(items)
+
+
+class Writer(_LineWriter):
+    def __init__(self, fname, relation='untitled', names=None):
+        self.fhand = open(fname, 'wb')
+        _LineWriter.__init__(self, relation, names)
+
+    def write(self, row):
+        for line in self.generate_lines(row):
+            line = line + os.linesep
+            self.fhand.write(line.encode('utf-8'))
+
+    def close(self):
+        self.fhand.close()
